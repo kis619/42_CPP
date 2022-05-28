f6eece39a75a0128b2531dce43c43d5dc92344fc
kis619 <kis619@abv.bg>
2022-05-28 20:28:16 +0200

    finished ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/28 19:45:23 by kmilchev          #+#    #+#             */
+/*   Updated: 2022/05/28 20:27:19 by kmilchev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string text = "HI THIS IS BRAIN";
+	std::string *stringPTR = &text;
+	std::string &stringREF = text;
+	
+	std::cout << "The memory address of the string variable: " << stringPTR << std::endl; 
+	std::cout << "The memory address of the string variable: " << &text << std::endl; 
+	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl; 
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl; 
+	std::cout << "_________________________________________" << std::endl;
+	std::cout << "The value of the string variable: " << text << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
