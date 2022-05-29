f992a943b2b6da28aca03f935a3ef1e85a5ef773
kis619 <kis619@abv.bg>
2022-05-29 02:58:11 +0200

    added new lines for readability

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 19:45:23 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/05/28 20:27:19 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/05/29 02:57:31 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ int main(void)
 	std::cout << "The memory address of the string variable: " << &text << std::endl; 
 	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl; 
 	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl; 
+	
 	std::cout << "_________________________________________" << std::endl;
+	
 	std::cout << "The value of the string variable: " << text << std::endl;
 	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
 	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
