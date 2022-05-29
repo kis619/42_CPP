7238683563acf8847fc22e85265bcded5f3e3bdb
kis619 <kis619@abv.bg>
2022-05-29 17:15:49 +0200

    chnaged status codes to 1 and 0

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmilchev <kmilchev@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 03:25:40 by kmilchev          #+#    #+#             */
-/*   Updated: 2022/05/29 17:11:29 by kmilchev         ###   ########.fr       */
+/*   Updated: 2022/05/29 17:15:13 by kmilchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		std::cout << "Punk!" << std::endl;
-		return (EXIT_FAILURE);
+		return (1);
 	}
 	
 	Harl harlito = Harl();
 	harlito.complain(argv[1]);
-	return (EXIT_SUCCESS);
+	return (0);
 }
